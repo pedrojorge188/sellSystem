74f138029c0cd438973b1e8e8c597eb1f4040fe3
pedrojorge188 <pedrorodriguesjorge03@gmail.com>
2022-08-30 17:01:15 +0100

    push update

--- sellSystem/sellSystem/sellSystem.py
+++ sellSystem/sellSystem/sellSystem.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
+list_tables = ['janeiro','fevereiro','marco','abril']
+
+for month in list_tables:
+    print(month)
+    readding_tables =  pd.read_excel(f'{month}.xlsx')
+    print(readding_tables)
+
+
+
+
+
 
